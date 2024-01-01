b5a797d6f4d3df2d3abb6b818630502a0b47604e
akhoa6204 <khoaanh662004@gmail.com>
2024-01-01 15:17:45 +0700

    first commit

---
+++ 1.py
@@ -0,0 +1,21 @@
+# Algorithm_02
+# Algorithm_07
+Tạo 1 hàm trong đó có đầu vào là ngày bắt đầu, ngày kết thúc và thời gian mong muốn được nhắc nhở trong ngày
+Trong hàm đó sẽ có 1 biến thu nhập dữ liệu thời gian 
+Nếu biến thu nhập dữ liệu thời gian đáp ứng được điều kiện đầu vào và không thuộc thứ 7 và chủ nhật 
+Thì nó sẽ đưa ra thông báo nhắc nhở
+# Algorithm_08
+# 1
+print('hello world')
+# 2 
+s = 'hello world' 
+print(s)
+# 3 
+a = 'hello'
+b = 'world' 
+c = a + ' ' + b 
+print(c)
+# 4 
+def Print(s): 
+    print(s)
+Print(c)
